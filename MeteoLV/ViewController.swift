8822d9b20cd35940c0807de52b5ba37f542a7c6c
Kristaps Grinbergs <kristaps@qminderapp.com>
2018-02-18 19:21:03 +0200

    Initial Commit

---
+++ MeteoLV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeteoLV
+//
+//  Created by Kristaps Grinbergs on 18/02/2018.
+//  Copyright © 2018 fassko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
