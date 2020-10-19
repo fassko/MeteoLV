2c7f9c3fd4b62b32a562972676bb7ba4843ca2a9
Kristaps Grinbergs <fassko@gmail.com>
2020-10-19 23:50:19 +0300

    Siri shortcuts support

---
+++ MeteoLVSiri/IntentHandler.swift
@@ -0,0 +1,42 @@
+//
+//  IntentHandler.swift
+//  MeteoLVSiri
+//
+//  Created by Kristaps Grinbergs on 19/10/2020.
+//  Copyright © 2020 fassko. All rights reserved.
+//
+
+import Intents
+
+import MeteoLVProvider
+
+class IntentHandler: INExtension {
+  override func handler(for intent: INIntent) -> Any {
+    SiriIntentHandler()
+  }
+}
+
+class SiriIntentHandler: NSObject, CurrentConditionsIntentHandling {
+  func handle(intent: CurrentConditionsIntent, completion: @escaping (CurrentConditionsIntentResponse) -> Void) {
+    guard let userDefaults = UserDefaults(suiteName: "group.com.fassko.MeteoLV"),
+          let home = userDefaults.string(forKey: "home") else {
+//      completion(.failure(error: "Please set the home location."))
+      return
+    }
+    
+    MeteoLVProvider().observations { result in
+      switch result {
+      case let .success(stations):
+        
+        guard let homeStation = stations.first(where: { $0.id == home }),
+              let temperature = homeStation.temperature else {
+          return
+        }
+        
+        completion(.success(temperature: temperature.replacingOccurrences(of: ",", with: ".")))
+      case let .failure(error):
+        debugPrint(error)
+      }
+    }
+  }
+}
